00b4ad8f6026b310c4ebb815a06921928e4f642a
IMNAP <leto99999@gmail.com>
2022-07-10 19:49:53 +0300

    upload file complete v2

--- src/index.js
+++ src/index.js
@@ -10,8 +10,6 @@ import logger from './util/logger.js';
 import multer from 'multer'
 
 
-
-
 dotenv.config();
 const PORT = process.env.SERVER_PORT || 3000;
 const app = express();
@@ -20,22 +18,26 @@ app.use(cors({
     origin: '*'
 }));
 
-const __dirname = path.resolve();
-app.use(express.static(__dirname + "/public"));
 
-// is it better to use defenite route to avoid overloading? 
-app.use(multer({dest:"public/uploads"}).single("filedata"));
+app.use(express.static(path.resolve() + "/public"));
 
+//TODO is it better to use defenite route to avoid overloading? 
+//--------------
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+})
 
+const upload = multer({storage: storage}).single("filedata");
+app.use(upload);
 
 app.use('/', userRouter)
 app.get('/', (req, res) => res.send(new ServerCustomResponse(HttpStatus.OK.code, HttpStatus.OK.status, 'API, v1.0.0 - All Systems Go',)));
 app.all('*', (req, res) => res.status(HttpStatus.NOT_FOUND.code)
   .send(new ServerCustomResponse(HttpStatus.NOT_FOUND.code, HttpStatus.NOT_FOUND.status, 'Route does not exist on the server')));
 app.listen(PORT, () => logger.info(`Server running on: ${ip.address()}:${PORT}`));
-
-
-
-// app.use(express.static(__dirname + '/public'));
-// app.use(express.json())
